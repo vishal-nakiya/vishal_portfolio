dd226e2209a289d6bdf3bed07466316fa589c2ca
NakiyaVishal <vishal.nakiya@tuvoc.com>
2024-01-20 19:39:56 +0530

    Updates

---
+++ precache-manifest.4579ca9b88a31ffc22dfa693b2244278.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd18c1c26eaf6a28f520e7ba43be63b",
+    "url": "/vishal_portfolio/index.html"
+  },
+  {
+    "revision": "c91af276f4b2ace87ac3",
+    "url": "/vishal_portfolio/static/css/main.e650b357.chunk.css"
+  },
+  {
+    "revision": "bf12045b1b3ca8a261b2",
+    "url": "/vishal_portfolio/static/js/2.ef6aa754.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/vishal_portfolio/static/js/2.ef6aa754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91af276f4b2ace87ac3",
+    "url": "/vishal_portfolio/static/js/main.ad88e358.chunk.js"
+  },
+  {
+    "revision": "eab9d1e11923a44fa474",
+    "url": "/vishal_portfolio/static/js/runtime-main.0689392f.js"
+  },
+  {
+    "revision": "80a80257157c3a6436157aca9b8a99f7",
+    "url": "/vishal_portfolio/static/media/Admin - Dashboard.80a80257.png"
+  },
+  {
+    "revision": "6ce24603f89ed54f3e4c49af521fbb4b",
+    "url": "/vishal_portfolio/static/media/Admin - Manage Users.6ce24603.png"
+  },
+  {
+    "revision": "e0251390d490033e2668d3b96f4bbf0f",
+    "url": "/vishal_portfolio/static/media/Admin - Manage Withdraw Requests.e0251390.png"
+  },
+  {
+    "revision": "66c71d6d14e0412d13aae57da7d237b8",
+    "url": "/vishal_portfolio/static/media/Dashboard (1).66c71d6d.png"
+  },
+  {
+    "revision": "020e178f38e1a30c62d8ea796ddc7a5b",
+    "url": "/vishal_portfolio/static/media/JEWEL_MAKER.020e178f.png"
+  },
+  {
+    "revision": "dc493c205c5d84ebe198a72a1074b581",
+    "url": "/vishal_portfolio/static/media/JEWEL_MAKERLogin.dc493c20.png"
+  },
+  {
+    "revision": "7b3ce55fb86f58fe049dad964c88fa3e",
+    "url": "/vishal_portfolio/static/media/Tuvoc-1.7b3ce55f.svg"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/vishal_portfolio/static/media/aws-2.fdff4014.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/vishal_portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/vishal_portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "9c517cee21e6bc67384619377ad5a7ea",
+    "url": "/vishal_portfolio/static/media/bitbucket-icon.9c517cee.svg"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/vishal_portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/vishal_portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/vishal_portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "faf8f499559f6b2f815f454a5794eae3",
+    "url": "/vishal_portfolio/static/media/flutter.faf8f499.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/vishal_portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/vishal_portfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/vishal_portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/vishal_portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "2fa5a6b6d7ce810d2b67ebf6df87fcc8",
+    "url": "/vishal_portfolio/static/media/laravel-2.2fa5a6b6.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/vishal_portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "3ea10fb644c05e7c947611dae8d58a45",
+    "url": "/vishal_portfolio/static/media/mern_tip_calc.3ea10fb6.webp"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/vishal_portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "b423766d4d3b2da3942977c207206446",
+    "url": "/vishal_portfolio/static/media/mysql-logo.b423766d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/vishal_portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/vishal_portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/vishal_portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/vishal_portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/vishal_portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/vishal_portfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "15ff424255016808e1caa37ab0313bf8",
+    "url": "/vishal_portfolio/static/media/wepik-export-20240114120421W9dd.15ff4242.png"
+  }
+]);
